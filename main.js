6308c3c0e69f3162d36255b32fad1b34dfa7951c
fangxiaohang <3462401687@qq.com>
2020-08-21 11:30:11 +0800

    修改部分代码  jQuery封装Ajax

--- main.js
+++ main.js
@@ -1,28 +1,50 @@
-myButton.addEventListener('click', (e) => {
+window.jQuery = function (nodeOrSelector) {
+    let nodes = {}
+    nodes.addClass = function () { }
+    nodes.html = function () { }
+    return nodes
+}
+window.$ = window.jQuery
+
+$.ajax = function ({ url, method, body, successFn, failFn, headers }) {
     let request = new XMLHttpRequest()
+    request.open(method, url) // 配置request
+    for (let key in headers) {
+        let value = headers[key]
+        request.setRequestHeader(key, value)
+    }
     request.onreadystatechange = () => {
         if (request.readyState === 4) {
-            console.log('请求响应都完毕了')
-            console.log(request.status)
-            console.log(request.statusText)
             if (request.status >= 200 && request.status < 300) {
-                console.log('说明请求成功')
-                console.log(request.getResponseHeader('Content-Type'))
-                console.log(request.responseText)
-                let string = request.responseText
-                //把符合 JSON 语法的字符串
-                //转化成 Js 对应的值
-                let object = window.JSON.parse(string)
-                document.body.innerHTML = object.note.from
-
+                successFn.call(undefined, request.responseText)
             } else if (request.status >= 400) {
-                console.log('说明请求失败')
+                failFn.call(undefined, request)
             }
         }
     }
-    request.open('POST', '/xxx', true);
-    request.setRequestHeader('fang', '20');
-    request.setRequestHeader('Content-type', 'x-www-form-urlencoded');
-    request.send('我偏要设置第四部分')
+    request.send(body)
+}
+
+function f1(responseText) { }
+function f2(responseText) { }
+
+myButton.addEventListener('click', (e) => {
+    window.jQuery.ajax({
+        url: '/fang',
+        method: 'get',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'fang': '20'
+        },
+        successFn: (x) => {
+            f1.call(undefined, x)
+            f2.call(undefined, x)
+        },
+        failFn: (x) => {
+            console.log(x)
+            console.log(x.status)
+            console.log(x.responseText)
+        }
+    })
 })
 
